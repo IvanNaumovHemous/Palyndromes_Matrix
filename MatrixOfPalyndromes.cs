ebe1729a6b36d9cdab315b86d4c186d6ae0b1e42
Ivan <ivan.naumov92@yahoo.com>
2018-06-24 17:26:55 +0300

    Import

---
+++ MatrixOfPalyndromes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace MatrixOfPalindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var r = int.Parse(input[0]);
+            var c = int.Parse(input[1]);
+
+            var matrix = GetMatrixOfPalyndromes(r, c);
+            printMatrix(matrix);
+        }
+
+        private static void printMatrix(string[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static string[,] GetMatrixOfPalyndromes(int r, int c)
+        {
+            var matrix = new string[r, c];
+            var alphabet = "abcdefghijklmnopqrstuvwxyz".ToList(); 
+
+            for (var i = 0; i < r; i++)
+            {
+                for (var j = 0; j < c; j++)
+                {
+                    var FirstAndLast = alphabet[0].ToString();
+                    var middle = alphabet[j].ToString();
+
+                    matrix[i, j] = FirstAndLast + middle + FirstAndLast;
+                }
+
+                alphabet.RemoveAt(0);
+            }
+
+            return matrix;
+        }
+    }
+}
